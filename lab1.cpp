f2d782ed749f45d330e4ed8c9a11ec943c7cbaa4
6704062611158 <s6704062611158@email.kmutnb.ac.th>
2024-11-28 15:05:43 +0700

    ข้อ2ใหม่

--- lab1.cpp
+++ lab1.cpp
@@ -4,9 +4,10 @@ int main(){
 	int n1;
 	int n2;
 	
-	printf("n1");
+	printf("Enter number1: ");
 	scanf("%d", &n1);
-	printf("n2");
+	
+	printf("Enter number2: ");
 	scanf("%d", &n2);
 	
     if (n1 > n2){
